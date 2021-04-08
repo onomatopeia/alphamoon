fc99c1e7c6485093528e532827891714bb0f8ad1
onomatopeia <agata.migalska@gmail.com>
2021-04-09 00:17:47 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='alphamoon',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recruitment task',
+    author='agata migalska',
+    license='',
+)

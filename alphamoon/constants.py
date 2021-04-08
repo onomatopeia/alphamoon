1752db773a905f8d9efb0b0d221ae311ab8bab73
onomatopeia <agata.migalska@gmail.com>
2021-04-09 01:54:31 +0200

    Exploratory data analysis

---
+++ alphamoon/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).resolve().parents[1]
+DATA_DIR = PROJECT_DIR / 'data'
+RAW_DATA_DIR = DATA_DIR / 'raw'
+PROCESSED_DATA_DIR = DATA_DIR / 'processed'
+REPORTS_DIR = PROJECT_DIR / 'reports'
+REPORTS_FIGURES_DIR = REPORTS_DIR / 'figures'

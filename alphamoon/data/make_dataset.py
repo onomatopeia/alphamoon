4bc1a9e5421b64413405c539acee80a70417862f
onomatopeia <agata.migalska@gmail.com>
2021-04-14 16:08:39 +0200

    Validation phase

--- alphamoon/data/make_dataset.py
+++ alphamoon/data/make_dataset.py
@@ -48,7 +48,9 @@ class TripletDataset(Dataset):
         indices_for_pos = np.where(y == y_anchor)[0]
         indices_for_neg = np.where(y != y_anchor)[0]
 
-        idx_pos = random.choice(indices_for_pos)
+        idx_pos = index
+        while idx_pos == index:
+            idx_pos = random.choice(indices_for_pos)
         idx_neg = random.choice(indices_for_neg)
 
         return x_anchor, X[idx_pos], X[idx_neg]
@@ -60,7 +62,17 @@ class TripletDataset(Dataset):
             return len(self.y_test)
 
 
-def get_data_loader(phase: Phase, X_train, y_train, X_test, y_test, batch_size: int = 10, shuffle: bool = True,
-                    num_workers: int = 0, pin_memory: bool = True):
-    return torch.utils.data.DataLoader(TripletDataset(X_train, y_train, X_test, y_test, phase), batch_size=batch_size,
-                                       shuffle=shuffle, num_workers=num_workers, pin_memory=pin_memory)
+def get_data_loaders(X_train, y_train, X_test, y_test, batch_size: int = 10, shuffle: bool = True,
+                     num_workers: int = 0, pin_memory: bool = True, train_fraction=0.8):
+    data_loaders = dict()
+    params = dict(batch_size=batch_size, shuffle=shuffle, num_workers=num_workers, pin_memory=pin_memory)
+    data_loaders[Phase.TEST] = torch.utils.data.DataLoader(TripletDataset(X_train, y_train, X_test, y_test, Phase.TEST),
+                                                           **params)
+    train_dataset = TripletDataset(X_train, y_train, X_test, y_test, Phase.TRAIN)
+    total = X_train.shape[0]
+    train_size = int(total * train_fraction)
+    validation_size = total - train_size
+    train_set, validation_set = torch.utils.data.random_split(train_dataset, [train_size, validation_size])
+    data_loaders[Phase.TRAIN] = torch.utils.data.DataLoader(train_set, **params)
+    data_loaders[Phase.VALIDATION] = torch.utils.data.DataLoader(validation_set, **params)
+    return data_loaders
--- alphamoon/models/train_model.py
+++ alphamoon/models/train_model.py
@@ -1,14 +1,24 @@
-import os
+import pickle
 
 import numpy as np
 import torch.cuda
-from alphamoon.data.make_dataset import Phase
+import torch.nn
+import torch.optim
+from scipy.ndimage import affine_transform
+from sklearn.model_selection import StratifiedShuffleSplit
+from tqdm import tqdm
+
+from alphamoon.constants import *
+from alphamoon.data.make_dataset import get_data_loaders, Phase
+from alphamoon.features.build_features import EmbeddingNet, TripletNet
 
 
-def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path, from_epoch=0, valid_loss_min=np.Inf):
+def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path='state_dict_model.pt',
+          from_epoch=0, valid_loss_min=np.Inf):
     """returns trained model"""
 
     for epoch in range(1, n_epochs + 1):
+        print(f'Epoch {epoch}')
         # initialize variables to monitor training and validation loss
         train_loss = 0.0
         valid_loss = 0.0
@@ -17,7 +27,7 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path, f
         # train the model #
         ###################
         model.train()
-        for batch_idx, (data, target) in enumerate(loaders[Phase.TRAIN]):
+        for batch_idx, (data, target) in enumerate(tqdm(loaders[Phase.TRAIN])):
             # move to GPU
             if use_cuda:
                 data = tuple(d.cuda() for d in data)
@@ -32,13 +42,13 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path, f
         ######################
         # validate the model #
         ######################
-        """model.eval()
-        for batch_idx, (data, target) in enumerate(loaders['valid']):
+        model.eval()
+        for batch_idx, (data, target) in enumerate(tqdm(loaders[Phase.VALIDATION])):
             # move to GPU
             if use_cuda:
-                data, target = data.cuda(), target.cuda()
-            output = model.forward(data)
-            loss = criterion(output, target)
+                data = tuple(d.cuda() for d in data)
+            output = model.forward(*data)
+            loss = criterion(*output)
             valid_loss += ((1 / (batch_idx + 1)) * (loss.data - valid_loss))
 
         # print training/validation statistics
@@ -55,17 +65,19 @@ def train(n_epochs, loaders, model, optimizer, criterion, use_cuda, save_path, f
                 save_model(model, save_path)
             except Exception as e:
                 print(f'Could not save the model due to {str(e)}')
-            valid_loss_min = valid_loss"""
+            valid_loss_min = valid_loss
 
     # return trained model
     return model
 
 
-def save_model(model, save_path, epoch=-1):
+def save_model(model, file_name='state_dict_model.pt', epoch=-1):
     if epoch > 0:
-        file_name, file_ext = os.path.splitext(save_path)
-        torch.save(model.state_dict(), f'{file_name}_{epoch}{file_ext}')
-    torch.save(model.state_dict(), save_path)
+        full_path = Path(__file__).resolve().parents[2] / 'models' / file_name
+        file_stem = full_path.stem
+        file_ext = full_path.suffix
+        torch.save(model.state_dict(), f'{file_stem}_{epoch}{file_ext}')
+    torch.save(model.state_dict(), file_name)
 
 
 def test(loaders, model, criterion, use_cuda):
@@ -97,9 +109,52 @@ def test(loaders, model, criterion, use_cuda):
         100. * correct / total, correct, total))
 
 
-"""if __name__ == '__main__':
-    model = TripletNet()
-    model_transfer = models.inception_v3(pretrained=True)
+if __name__ == '__main__':
+
+    def get_transformation_matrix(img_w_h):
+        theta = np.deg2rad(-7)
+        ry = rx = img_w_h / 2
+
+        r00 = np.cos(theta)
+        r01 = -np.sin(theta)
+        r10 = np.sin(theta)
+        r11 = np.cos(theta)
+        r02 = rx - r00 * rx - r01 * ry
+        r12 = ry - r10 * rx - r11 * ry
+
+        rotation = np.array([[r00, r01, r02], [r10, r11, r12], [0, 0, 1]])
+
+        Sx = 0.9
+        Sy = 1.1
+        Tx = -5
+        Ty = 2
+
+        affine2 = np.array([[Sy, 0, Tx], [0, Sx, Ty], [0, 0, 1]])
+        trans = np.matmul(rotation, affine2)
+        return trans
+
+    input_data_path = RAW_DATA_DIR / 'train.pkl'
+    with input_data_path.open('rb') as file_handle:
+        X, y = pickle.load(file_handle)
+
+    one_shot_example = np.where(y[:, 0] == 30)[0][0]
+
+    img_w_h = int(np.sqrt(X.shape[1]))
+    trans = get_transformation_matrix(img_w_h)
+    x2 = affine_transform(np.reshape(X[one_shot_example], (img_w_h, img_w_h)), trans)
+    x2_ravel = np.ravel(x2)
+    X_augmented = np.vstack((X, x2_ravel))
+    y_augmented = np.vstack((y, [30]))
+
+    ss = StratifiedShuffleSplit(n_splits=1, test_size=0.33, random_state=0)
+    for train_index, test_index in ss.split(X_augmented, y_augmented):
+        X_train, X_test = X_augmented[train_index], X_augmented[test_index]
+        y_train, y_test = y_augmented[train_index], y_augmented[test_index]
+
+    embedding_model = EmbeddingNet(X_train.shape[1], 64, 64)
+    model = TripletNet(embedding_model)
+
+    """model_transfer = models.inception_v3(pretrained=True)
     model_transfer = freeze_weights(model_transfer)
     model_transfer.fc = create_fc_layer(model_transfer.fc, no_classes)
     model_transfer.AuxLogits.fc = create_fc_layer(model_transfer.AuxLogits.fc, no_classes)
@@ -113,10 +168,6 @@ def test(loaders, model, criterion, use_cuda):
     if use_cuda:
         model_transfer = model_transfer.cuda()
 
-    opt_SGD = 'SGD'
-    opt_Adam = 'Adam'
-    opt_RMSprop = 'RMSprop'
-
     criterion_transfer = nn.CrossEntropyLoss()
 
     lr = 0.01
@@ -125,26 +176,21 @@ def test(loaders, model, criterion, use_cuda):
     trainable_params = get_trainable_parameters([model_transfer.fc, model_transfer.AuxLogits.fc])
     print(len(trainable_params))
     optimizer_transfer = get_optimizer(opt_class, trainable_params, lr)
-
-    # get data loaders
-
-    dog_images_dir = os.path.join('data', 'dogImages')
-    train_dir = os.path.join(dog_images_dir, 'train')
-    valid_dir = os.path.join(dog_images_dir, 'valid')
-    test_dir = os.path.join(dog_images_dir, 'test')
-
-    loaders_transfer = dict(train=get_data_loader(train_dir, 'train', 224),
-                           valid=get_data_loader(valid_dir, 'valid', 224),
-                           test=get_data_loader(test_dir, 'test', 224))
+    """
 
     # train the model
+    n_epochs = 10
+    margin = 1
+    use_cuda = True  # torch.cuda.is_available()
+    if use_cuda:
+        model.cuda()
+    loss_fn = torch.nn.TripletMarginWithDistanceLoss(
+        distance_function=torch.nn.PairwiseDistance(), margin=margin)
+    optimizer = torch.optim.Adam(model.parameters())
 
-    n_epochs = 100
-    suffix = f'_{opt_class}_{n_epochs}_{lr}'
-    model_transfer = train(n_epochs, loaders_transfer, model_transfer, optimizer_transfer,
-                           criterion_transfer, use_cuda, f'model_transfer_{suffix}.pt')
+    loaders = get_data_loaders(X_train, y_train, X_test, y_test)
+    train(n_epochs, loaders, model, optimizer, loss_fn, use_cuda)
 
     # load the model that got the best validation accuracy (uncomment the line below)
-    model_transfer.load_state_dict(torch.load(f'model_transfer_{suffix}.pt'))
-    test(loaders_transfer, model_transfer, criterion_transfer, use_cuda)
-"""
+    # model_transfer.load_state_dict(torch.load(f'model_transfer_{suffix}.pt'))
+    # test(loaders_transfer, model_transfer, criterion_transfer, use_cuda)
